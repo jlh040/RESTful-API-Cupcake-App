381f48ae82bbe886f28fe462e43f46cd752fe782
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-05 03:57:33 -0700

    the user can now create cupcakes

--- static/app.js
+++ static/app.js
@@ -1,4 +1,15 @@
-const BASE_URL = '/api/cupcakes'
+const BASE_URL = '/api/cupcakes';
+const submitButton = $('#submit-button');
+
+putCardsOnPage(getCupcakeArr());
+
+submitButton.click(async function(e) {
+    e.preventDefault();
+    await sendDataToApi(getDataFromForm());
+    $('#cupcakes-list').empty()
+    putCardsOnPage(await getCupcakeArr());
+    emptyInputs();
+})
 
 async function getCupcakeArr() {
     resp = await axios.get(BASE_URL);
@@ -7,7 +18,7 @@ async function getCupcakeArr() {
 
 function createCardHtml(flavor, size, rating, image_url) {
     return  `
-            <div class="card d-inline-block ms-3" style="width: 18rem;">
+            <div class="card d-inline-block ms-3 mt-3" style="width: 18rem;">
                 <img src="${image_url}" class="card-img-top">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">Flavor: ${flavor}</li>
@@ -18,8 +29,10 @@ function createCardHtml(flavor, size, rating, image_url) {
             `
 };
 
-async function putCardOnPage(arrOfCupcakes) {
+async function putCardsOnPage(arrOfCupcakes) {
     const $cupcakeList = $('#cupcakes-list')
+    $cupcakeList.empty()
+
     for (let obj of await arrOfCupcakes) {
         $cupcakeList.append(
             createCardHtml(obj.flavor, obj.size, obj.rating, obj.image)
@@ -27,5 +40,42 @@ async function putCardOnPage(arrOfCupcakes) {
     };
 };
 
-putCardOnPage(getCupcakeArr());
+function getDataFromForm() {
+    const flavor = $('#flavor').val();
+    const size = $('#size').val();
+    const rating = $('#rating').val();
+    const image = $('#image').val() ? $('#image').val() : null;
+
+    return {
+        flavor,
+        size,
+        rating,
+        image
+    }
+}
+
+function emptyInputs() {
+    $('#flavor').val('');
+    $('#size').val('');
+    $('#rating').val('');
+    $('#image').val('')
+
+    return
+}
+
+async function sendDataToApi(obj) {
+    await axios.post(
+        BASE_URL,
+        obj
+    )
+}
+
+
+
+
+
+
+
+
+
 
