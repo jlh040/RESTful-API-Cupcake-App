f0873343b3b24b8b024bb2e47521ec418c75da71
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-04 23:24:06 -0700

    created the GET all cupcakes route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Flask app for Cupcakes"""
 from flask import Flask, render_template, redirect, flash, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
@@ -14,3 +14,9 @@ connect_db(app)
 app.config['SECRET_KEY'] = '3nc7v9a1x'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+@app.route('/api/cupcakes', methods = ['GET'])
+def get_all_cupcakes():
+    """Get all cupcakes from the db."""
+    all_cakes = [cupcake.serialize_cupcake() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cakes)
--- models.py
+++ models.py
@@ -17,3 +17,14 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable = False)
     rating = db.Column(db.Float, nullable = False)
     image = db.Column(db.Text, nullable = False, default = 'https://tinyurl.com/demo-cupcake')
+
+    def serialize_cupcake(self):
+        """Turn cupcake instance into a JSONifiable dictionary."""
+
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image': self.image
+        }
