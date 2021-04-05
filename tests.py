ad6f5651ef74ca8f2197934d0d18e7b7c76be6bc
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-05 01:12:59 -0700

    created test for the DELETE route

--- tests.py
+++ tests.py
@@ -124,3 +124,12 @@ class CupcakeViewsTestCase(TestCase):
                     "image": "http://test.com/cupcake.jpg"
                 }
             })
+    
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+
+            self.assertEqual(resp.status_code, 200)
+            
+            self.assertEqual(Cupcake.query.count(), 0)
