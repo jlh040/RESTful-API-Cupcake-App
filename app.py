f89257d42340fd0e3fe60f1b2419c76d72a4a67c
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-04 22:36:35 -0700

    added basic boilerplate for app.py and models.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, flash, jsonify, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cupcakes'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = '3nc7v9a1x'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,1 +1,10 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
